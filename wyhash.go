4d61f90b4dc0b8879500962e0dd10e1f2c5a09d0
Damian Gryski <damian@gryski.com>
2019-03-11 10:43:49 -0700

    remove uneeded break statements

--- wyhash.go
+++ wyhash.go
@@ -39,100 +39,68 @@ func Hash(key []byte, seed uint64) uint64 {
 	switch len(key) & 31 {
 	case 0:
 		seed = wyhashmix(seed^wyhashp1, wyhashread64(ptr)) ^ wyhashmix(seed^wyhashp2, wyhashread64(ptr[8:])) ^ wyhashmix(seed^wyhashp3, wyhashread64(ptr[16:])) ^ wyhashmix(seed^wyhashp4, wyhashread64(ptr[24:]))
-		break
 	case 1:
 		seed = wyhashmix(seed^wyhashp1, wyhashread08(ptr))
-		break
 	case 2:
 		seed = wyhashmix(seed^wyhashp1, wyhashread16(ptr))
-		break
 	case 3:
 		seed = wyhashmix(seed^wyhashp1, (wyhashread16(ptr)<<8)|wyhashread08(ptr[2:]))
-		break
 	case 4:
 		seed = wyhashmix(seed^wyhashp1, wyhashread32(ptr))
-		break
 	case 5:
 		seed = wyhashmix(seed^wyhashp1, (wyhashread32(ptr)<<8)|wyhashread08(ptr[4:]))
-		break
 	case 6:
 		seed = wyhashmix(seed^wyhashp1, (wyhashread32(ptr)<<16)|wyhashread16(ptr[4:]))
-		break
 	case 7:
 		seed = wyhashmix(seed^wyhashp1, (wyhashread32(ptr)<<24)|(wyhashread16(ptr[4:])<<8)|wyhashread08(ptr[6:]))
-		break
 	case 8:
 		seed = wyhashmix(seed^wyhashp1, wyhashread64(ptr))
-		break
 	case 9:
 		seed = wyhashmix(seed^wyhashp1, wyhashread64(ptr)) ^ wyhashmix(seed^wyhashp2, wyhashread08(ptr[8:]))
-		break
 	case 10:
 		seed = wyhashmix(seed^wyhashp1, wyhashread64(ptr)) ^ wyhashmix(seed^wyhashp2, wyhashread16(ptr[8:]))
-		break
 	case 11:
 		seed = wyhashmix(seed^wyhashp1, wyhashread64(ptr)) ^ wyhashmix(seed^wyhashp2, (wyhashread16(ptr[8:])<<8)|wyhashread08(ptr[8+2:]))
-		break
 	case 12:
 		seed = wyhashmix(seed^wyhashp1, wyhashread64(ptr)) ^ wyhashmix(seed^wyhashp2, wyhashread32(ptr[8:]))
-		break
 	case 13:
 		seed = wyhashmix(seed^wyhashp1, wyhashread64(ptr)) ^ wyhashmix(seed^wyhashp2, (wyhashread32(ptr[8:])<<8)|wyhashread08(ptr[8+4:]))
-		break
 	case 14:
 		seed = wyhashmix(seed^wyhashp1, wyhashread64(ptr)) ^ wyhashmix(seed^wyhashp2, (wyhashread32(ptr[8:])<<16)|wyhashread16(ptr[8+4:]))
-		break
 	case 15:
 		seed = wyhashmix(seed^wyhashp1, wyhashread64(ptr)) ^ wyhashmix(seed^wyhashp2, (wyhashread32(ptr[8:])<<24)|(wyhashread16(ptr[8+4:])<<8)|wyhashread08(ptr[8+6:]))
-		break
 	case 16:
 		seed = wyhashmix(seed^wyhashp1, wyhashread64(ptr)) ^ wyhashmix(seed^wyhashp2, wyhashread64(ptr[8:]))
-		break
 	case 17:
 		seed = wyhashmix(seed^wyhashp1, wyhashread64(ptr)) ^ wyhashmix(seed^wyhashp2, wyhashread64(ptr[8:])) ^ wyhashmix(seed^wyhashp3, wyhashread08(ptr[16:]))
-		break
 	case 18:
 		seed = wyhashmix(seed^wyhashp1, wyhashread64(ptr)) ^ wyhashmix(seed^wyhashp2, wyhashread64(ptr[8:])) ^ wyhashmix(seed^wyhashp3, wyhashread16(ptr[16:]))
-		break
 	case 19:
 		seed = wyhashmix(seed^wyhashp1, wyhashread64(ptr)) ^ wyhashmix(seed^wyhashp2, wyhashread64(ptr[8:])) ^ wyhashmix(seed^wyhashp3, (wyhashread16(ptr[16:])<<8)|wyhashread08(ptr[16+2:]))
-		break
 	case 20:
 		seed = wyhashmix(seed^wyhashp1, wyhashread64(ptr)) ^ wyhashmix(seed^wyhashp2, wyhashread64(ptr[8:])) ^ wyhashmix(seed^wyhashp3, wyhashread32(ptr[16:]))
-		break
 	case 21:
 		seed = wyhashmix(seed^wyhashp1, wyhashread64(ptr)) ^ wyhashmix(seed^wyhashp2, wyhashread64(ptr[8:])) ^ wyhashmix(seed^wyhashp3, (wyhashread32(ptr[16:])<<8)|wyhashread08(ptr[16+4:]))
-		break
 	case 22:
 		seed = wyhashmix(seed^wyhashp1, wyhashread64(ptr)) ^ wyhashmix(seed^wyhashp2, wyhashread64(ptr[8:])) ^ wyhashmix(seed^wyhashp3, (wyhashread32(ptr[16:])<<16)|wyhashread16(ptr[16+4:]))
-		break
 	case 23:
 		seed = wyhashmix(seed^wyhashp1, wyhashread64(ptr)) ^ wyhashmix(seed^wyhashp2, wyhashread64(ptr[8:])) ^ wyhashmix(seed^wyhashp3, (wyhashread32(ptr[16:])<<24)|(wyhashread16(ptr[16+4:])<<8)|wyhashread08(ptr[16+6:]))
-		break
 	case 24:
 		seed = wyhashmix(seed^wyhashp1, wyhashread64(ptr)) ^ wyhashmix(seed^wyhashp2, wyhashread64(ptr[8:])) ^ wyhashmix(seed^wyhashp3, wyhashread64(ptr[16:]))
-		break
 	case 25:
 		seed = wyhashmix(seed^wyhashp1, wyhashread64(ptr)) ^ wyhashmix(seed^wyhashp2, wyhashread64(ptr[8:])) ^ wyhashmix(seed^wyhashp3, wyhashread64(ptr[16:])) ^ wyhashmix(seed^wyhashp4, wyhashread08(ptr[24:]))
-		break
 	case 26:
 		seed = wyhashmix(seed^wyhashp1, wyhashread64(ptr)) ^ wyhashmix(seed^wyhashp2, wyhashread64(ptr[8:])) ^ wyhashmix(seed^wyhashp3, wyhashread64(ptr[16:])) ^ wyhashmix(seed^wyhashp4, wyhashread16(ptr[24:]))
-		break
 	case 27:
 		seed = wyhashmix(seed^wyhashp1, wyhashread64(ptr)) ^ wyhashmix(seed^wyhashp2, wyhashread64(ptr[8:])) ^ wyhashmix(seed^wyhashp3, wyhashread64(ptr[16:])) ^ wyhashmix(seed^wyhashp4, (wyhashread16(ptr[24:])<<8)|wyhashread08(ptr[24+2:]))
-		break
 	case 28:
 		seed = wyhashmix(seed^wyhashp1, wyhashread64(ptr)) ^ wyhashmix(seed^wyhashp2, wyhashread64(ptr[8:])) ^ wyhashmix(seed^wyhashp3, wyhashread64(ptr[16:])) ^ wyhashmix(seed^wyhashp4, wyhashread32(ptr[24:]))
-		break
 	case 29:
 		seed = wyhashmix(seed^wyhashp1, wyhashread64(ptr)) ^ wyhashmix(seed^wyhashp2, wyhashread64(ptr[8:])) ^ wyhashmix(seed^wyhashp3, wyhashread64(ptr[16:])) ^ wyhashmix(seed^wyhashp4, (wyhashread32(ptr[24:])<<8)|wyhashread08(ptr[24+4:]))
-		break
 	case 30:
 		seed = wyhashmix(seed^wyhashp1, wyhashread64(ptr)) ^ wyhashmix(seed^wyhashp2, wyhashread64(ptr[8:])) ^ wyhashmix(seed^wyhashp3, wyhashread64(ptr[16:])) ^ wyhashmix(seed^wyhashp4, (wyhashread32(ptr[24:])<<16)|wyhashread16(ptr[24+4:]))
-		break
 	case 31:
 		seed = wyhashmix(seed^wyhashp1, wyhashread64(ptr)) ^ wyhashmix(seed^wyhashp2, wyhashread64(ptr[8:])) ^ wyhashmix(seed^wyhashp3, wyhashread64(ptr[16:])) ^ wyhashmix(seed^wyhashp4, (wyhashread32(ptr[24:])<<24)|(wyhashread16(ptr[24+4:])<<8)|wyhashread08(ptr[24+6:]))
-		break
 	}
 	return wyhashmix(seed, uint64(len(key)))
 }
