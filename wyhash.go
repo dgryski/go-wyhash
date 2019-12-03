3c7dba27294911fdafd024942176fb9081ff75f8
Damian Gryski <damian@gryski.com>
2019-12-03 12:16:31 -0800

    remove some bounds checks

--- wyhash.go
+++ wyhash.go
@@ -24,15 +24,15 @@ func wyr3(p []byte, k int) uint64 {
 }
 
 func wyr4(p []byte) uint64 {
-	return uint64(binary.LittleEndian.Uint32(p))
+	return uint64(binary.LittleEndian.Uint32(p[:4]))
 }
 
 func wyr8(p []byte) uint64 {
-	return uint64(binary.LittleEndian.Uint64(p))
+	return uint64(binary.LittleEndian.Uint64(p[:8]))
 }
 
 func wyr8mix(p []byte) uint64 {
-	return uint64(binary.LittleEndian.Uint32(p))<<32 | uint64(binary.LittleEndian.Uint32(p[4:]))
+	return uint64(binary.LittleEndian.Uint32(p[:4]))<<32 | uint64(binary.LittleEndian.Uint32(p[4:8]))
 }
 
 func Hash(key []byte, seed uint64) uint64 {
@@ -57,9 +57,8 @@ func Hash(key []byte, seed uint64) uint64 {
 	}
 
 	see1 := seed
-	i := len(p)
 
-	for i > 256 {
+	for len(p) > 256 {
 		seed = wymum(wyr8(p)^seed^wyp0, wyr8(p[8:])^seed^wyp1) ^ wymum(wyr8(p[16:])^seed^wyp2, wyr8(p[24:])^seed^wyp3)
 		see1 = wymum(wyr8(p[32:])^see1^wyp1, wyr8(p[40:])^see1^wyp2) ^ wymum(wyr8(p[48:])^see1^wyp3, wyr8(p[56:])^see1^wyp0)
 		seed = wymum(wyr8(p[64:])^seed^wyp0, wyr8(p[72:])^seed^wyp1) ^ wymum(wyr8(p[80:])^seed^wyp2, wyr8(p[88:])^seed^wyp3)
@@ -68,30 +67,28 @@ func Hash(key []byte, seed uint64) uint64 {
 		see1 = wymum(wyr8(p[160:])^see1^wyp1, wyr8(p[168:])^see1^wyp2) ^ wymum(wyr8(p[176:])^see1^wyp3, wyr8(p[184:])^see1^wyp0)
 		seed = wymum(wyr8(p[192:])^seed^wyp0, wyr8(p[200:])^seed^wyp1) ^ wymum(wyr8(p[208:])^seed^wyp2, wyr8(p[216:])^seed^wyp3)
 		see1 = wymum(wyr8(p[224:])^see1^wyp1, wyr8(p[232:])^see1^wyp2) ^ wymum(wyr8(p[240:])^see1^wyp3, wyr8(p[248:])^see1^wyp0)
-		i -= 256
 		p = p[256:]
 	}
 
-	for i > 32 {
+	for len(p) > 32 {
 		seed = wymum(wyr8(p)^seed^wyp0, wyr8(p[8:])^seed^wyp1)
 		see1 = wymum(wyr8(p[16:])^see1^wyp2, wyr8(p[24:])^see1^wyp3)
-		i -= 32
 		p = p[32:]
 	}
 
 	switch {
-	case i < 4:
-		seed = wymum(wyr3(p, i)^seed^wyp0, seed^wyp1)
-	case (i <= 8):
-		seed = wymum(wyr4(p)^seed^wyp0, wyr4(p[i-4:])^seed^wyp1)
-	case (i <= 16):
-		seed = wymum(wyr8mix(p)^seed^wyp0, wyr8mix(p[i-8:])^seed^wyp1)
-	case (i <= 24):
+	case len(p) < 4:
+		seed = wymum(wyr3(p, len(p))^seed^wyp0, seed^wyp1)
+	case (len(p) <= 8):
+		seed = wymum(wyr4(p)^seed^wyp0, wyr4(p[len(p)-4:])^seed^wyp1)
+	case (len(p) <= 16):
+		seed = wymum(wyr8mix(p)^seed^wyp0, wyr8mix(p[len(p)-8:])^seed^wyp1)
+	case (len(p) <= 24):
 		seed = wymum(wyr8mix(p)^seed^wyp0, wyr8mix(p[8:])^seed^wyp1)
-		see1 = wymum(wyr8mix(p[i-8:])^see1^wyp2, see1^wyp3)
+		see1 = wymum(wyr8mix(p[len(p)-8:])^see1^wyp2, see1^wyp3)
 	default:
 		seed = wymum(wyr8mix(p)^seed^wyp0, wyr8mix(p[8:])^seed^wyp1)
-		see1 = wymum(wyr8mix(p[16:])^see1^wyp2, wyr8mix(p[i-8:])^see1^wyp3)
+		see1 = wymum(wyr8mix(p[16:])^see1^wyp2, wyr8mix(p[len(p)-8:])^see1^wyp3)
 
 	}
 
